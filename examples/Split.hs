e07573a956b201f5ff7f319cafa52da1cc9b867a
Harendra Kumar <harendra@composewell.com>
2021-04-17 17:00:02 +0530

    Refactor imports, add comments

--- examples/Split.hs
+++ examples/Split.hs
@@ -1,23 +1,21 @@
-import qualified Streamly.Prelude as Stream
-import qualified Streamly.Internal.Data.Stream.IsStream as Stream
-       (chunksOf2, evalStateT)
-import qualified Streamly.FileSystem.Handle as FH
-import qualified Streamly.Internal.FileSystem.Handle as FH (write2)
-import qualified System.IO as FH
-
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.State.Strict (StateT(..), get, put)
-import System.Environment (getArgs)
-import System.IO (IOMode(..))
 import Data.Function ((&))
+import System.Environment (getArgs)
+import System.IO (Handle, IOMode(..), openFile, hClose)
 
-newHandle :: StateT (Maybe (FH.Handle, Int)) IO FH.Handle
+import qualified Streamly.Prelude as Stream
+import qualified Streamly.Internal.Data.Stream.IsStream as Stream (chunksOf2, evalStateT)
+import qualified Streamly.FileSystem.Handle as Handle
+import qualified Streamly.Internal.FileSystem.Handle as Handle (write2)
+
+newHandle :: StateT (Maybe (Handle, Int)) IO Handle
 newHandle = do
     old <- get
     idx <- case old of
             Nothing -> return 0
-            Just (h, i) -> liftIO (FH.hClose h) >> return (i + 1)
-    h <- liftIO $ FH.openFile ("dst-xyz-" ++ show idx ++ ".txt") WriteMode
+            Just (h, i) -> liftIO (hClose h) >> return (i + 1)
+    h <- liftIO $ openFile ("output-" ++ show idx ++ ".txt") WriteMode
     put (Just (h, idx))
     return h
 
@@ -25,16 +23,16 @@ newHandle = do
 -- The fold can return the file name/handle after it is done.
 -- similarly the files can written to directories and we can generate a stream
 -- of directory names.
-splitFile :: FH.Handle -> IO ()
+splitFile :: Handle -> IO ()
 splitFile inHandle =
-      Stream.unfold FH.read inHandle
-    & Stream.liftInner
-    & Stream.chunksOf2 (180 * 1024 * 1024) newHandle FH.write2
-    & Stream.evalStateT (return Nothing)  -- generate new handle for each iteration
-    & Stream.drain
+      Stream.unfold Handle.read inHandle -- SerialT IO Word8
+    & Stream.liftInner -- SerialT (StateT (Maybe (Handle, Int)) IO) Word8
+    & Stream.chunksOf2 (180 * 1024 * 1024) newHandle Handle.write2 -- SerialT (StateT (Maybe (Handle, Int)) IO) ()
+    & Stream.evalStateT (return Nothing)  -- SerialT IO ()
+    & Stream.drain                        -- SerialT IO ()
 
 main :: IO ()
 main = do
     name <- fmap head getArgs
-    src <- FH.openFile name ReadMode
+    src <- openFile name ReadMode
     splitFile src
