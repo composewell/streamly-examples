da95ca52e3d46420ee9e8d8c49a979524ef652d5
Harendra Kumar <harendra@composewell.com>
2021-04-18 17:01:50 +0530

    Use sdl2
    Change CI from xenial to bionic for libsdl2-2.0.6+
    
    Add gpg key before installing nodesource for ghcjs CI

--- examples/CirclingSquare.hs
+++ examples/CirclingSquare.hs
@@ -2,68 +2,69 @@
 -- Displays a square moving in a circle. To move the position drag it with the
 -- mouse.
 
+import Control.Concurrent (forkIO)
 import Data.Function ((&))
 import Data.IORef (IORef, newIORef, readIORef, writeIORef)
-import Graphics.UI.SDL as SDL
-import Streamly.Prelude as Stream
+import SDL (($=), V2(..), V4(..), Point(..))
+
+import qualified Data.Text as Text
+import qualified SDL
+import qualified Streamly.Prelude as Stream
 
 ------------------------------------------------------------------------------
 -- SDL Graphics Init
 ------------------------------------------------------------------------------
 
-sdlInit :: IO ()
+sdlInit :: IO (SDL.Window, SDL.Renderer)
 sdlInit = do
-  SDL.init [InitVideo]
-
-  let width  = 640
-      height = 480
-  _ <- SDL.setVideoMode width height 16 [SWSurface]
-  SDL.setCaption "Test" ""
-
-------------------------------------------------------------------------------
--- Display a box at a given coordinates
-------------------------------------------------------------------------------
-
-display :: (Double, Double) -> IO ()
-display (playerX, playerY) = do
-  screen <- getVideoSurface
-
-  -- Paint screen green
-  let format = surfaceGetPixelFormat screen
-  bgColor <- mapRGB format 55 60 64
-  _ <- fillRect screen Nothing bgColor
-
-  -- Paint small red square, at an angle 'angle' with respect to the center
-  foreC <- mapRGB format 212 108 73
-  let side = 20
-      x = round playerX
-      y = round playerY
-  _ <- fillRect screen (Just (Rect x y side side)) foreC
-
-  -- Double buffering
-  SDL.flip screen
+    SDL.initializeAll
+    window <- SDL.createWindow (Text.pack "Circling Square") SDL.defaultWindow
+    rend <- SDL.createRenderer window (-1) SDL.defaultRenderer
+    return (window, rend)
 
 ------------------------------------------------------------------------------
 -- Wait and update Controller Position if it changes
 ------------------------------------------------------------------------------
 
-updateController :: IORef (Double, Double) -> IO ()
-updateController ref = do
-    e <- pollEvent
-    case e of
-        MouseMotion x y _ _ -> writeIORef ref (fromIntegral x, fromIntegral y)
-        _ -> return ()
+handleEvents :: IORef (Double, Double) -> IO Bool
+handleEvents ref = do
+    e <- SDL.waitEvent
+    case SDL.eventPayload e of
+        SDL.MouseMotionEvent (SDL.MouseMotionEventData _ _ _ (P (V2 x y)) _) -> do
+            writeIORef ref (fromIntegral x, fromIntegral y)
+            return True
+        SDL.WindowClosedEvent _ ->
+            return False
+        _ -> return True
+
+------------------------------------------------------------------------------
+-- Display a rectangle at given coordinates
+------------------------------------------------------------------------------
+
+display :: (Double, Double) -> SDL.Renderer -> IO ()
+display (playerX, playerY) rend = do
+  -- Paint the screen with background color
+  SDL.rendererDrawColor rend $= V4 55 60 64 255
+  SDL.fillRect rend Nothing
+
+  -- Paint a small square at the current position
+  SDL.rendererDrawColor rend $= V4 212 108 73 255
+  let diag = 15
+      x = round playerX
+      y = round playerY
+  SDL.fillRect rend (Just (SDL.Rectangle (P (V2 x y)) diag))
 
 ------------------------------------------------------------------------------
 -- Periodically refresh the output display
 ------------------------------------------------------------------------------
 
-updateDisplay :: IORef (Double, Double) -> IO ()
-updateDisplay ref = do
-    time <- SDL.getTicks
+updateDisplay :: IORef (Double, Double) -> SDL.Renderer -> IO ()
+updateDisplay ref rend = do
+    time <- SDL.ticks
     (x, y) <- readIORef ref
     let t = fromIntegral time * speed / 1000
-     in display (x + cos t * radius, y + sin t * radius)
+     in display (x + cos t * radius, y + sin t * radius) rend
+    SDL.present rend
 
     where
 
@@ -72,12 +73,14 @@ updateDisplay ref = do
 
 main :: IO ()
 main = do
-    sdlInit
+    (window, rend) <- sdlInit
     ref <- newIORef (0,0)
-    let mouseStream = Stream.repeatM (updateController ref)
-        displayStream = Stream.repeatM (updateDisplay ref)
 
-    mouseStream `Stream.parallel` displayStream -- AsyncT IO ()
-        & Stream.constRate 40                   -- AsyncT IO ()
-        & Stream.fromAsync                      -- SerialT IO ()
-        & Stream.drain                          -- IO ()
+    _ <- forkIO $ do
+            Stream.repeatM (updateDisplay ref rend) -- SerialT IO ()
+                & Stream.delay (1/60)               -- SerialT IO ()
+                & Stream.drain                      -- IO ()
+
+    -- MacOS requires pollEvents to run in the Main thread.
+    Stream.repeatM (handleEvents ref) & Stream.drainWhile (== True)
+    SDL.destroyWindow window
