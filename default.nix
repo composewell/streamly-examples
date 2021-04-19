d978abc215693f62e802944a0c41f25b4bd3a640
Harendra Kumar <harendra@composewell.com>
2021-04-20 01:41:56 +0530

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,112 @@
+# CAUTION! a spelling mistake in arg string is ignored silently.
+#
+# nix-shell --argstr c2nix "--flag examples-sdl"
+
+# To use ghc-8.6.5
+# nix-shell --argstr compiler "ghc865"
+
+{
+  nixpkgs ?
+    import (builtins.fetchTarball https://github.com/composewell/nixpkgs/archive/01dd2b4e738.tar.gz)
+        # fusion-plugin is marked as broken
+        { config.allowBroken = true;}
+, compiler ? "default"
+, c2nix ? "" # cabal2nix CLI options
+# TODO
+#, sources ? [] # e.g. [./. ./benchmark]
+#, hdeps ? [] # e.g. [time, mtl]
+#, deps ? [] # e.g. [SDL2]
+}:
+let haskellPackages =
+        if compiler == "default"
+        then nixpkgs.haskellPackages
+        else nixpkgs.haskell.packages.${compiler};
+
+    # we can possibly avoid adding our package to HaskellPackages like
+    # in the case of nix-shell for a single package?
+    mkPackage = super: pkg: path: opts: inShell:
+                let orig = super.callCabal2nixWithOptions pkg path opts {};
+                 in if inShell
+                    # Avoid copying the source directory to nix store by using
+                    # src = null.
+                    then orig.overrideAttrs (oldAttrs: { src = null; })
+                    else orig;
+
+    flags = "--flag fusion-plugin" + " " + c2nix;
+
+    mkHaskellPackages = inShell:
+        haskellPackages.override {
+            # We could disbale doCheck on all like this, but it would make the
+            # whole world rebuild, we can't use the binary cache
+            #packageSetConfig = self: super: {
+            #    mkDerivation = drv: super.mkDerivation (drv // {
+            #        doCheck = false;
+            #    });
+            #};
+            overrides = self: super:
+                with nixpkgs.haskell.lib;
+                {
+                    streamly-examples =
+                        mkPackage super "streamly-examples"
+                            ./. flags inShell;
+
+                    fusion-plugin =
+                      super.callHackageDirect
+                        { pkg = "fusion-plugin";
+                          ver = "0.2.2";
+                          sha256 = "1g7j50wa7ffyizv6fdnr09dr4w7bc0hskb46h7y0paw6rh9yz4zj";
+                        } {};
+
+                    # Example to Use a different version of a package
+                    #QuickCheck = self.QuickCheck_2_14;
+
+                    # Example to disable tests if tests fail or take too long
+                    # or to use different configure flags if needed
+                    #
+                    # XXX We need the ability to disable doCheck on all
+                    # those packages that are being built locally and
+                    # not fetched from the cache. Running tests could do
+                    # nasty things to the machine e.g. some tests even
+                    # listen for incoming connections on the network.
+                    #selective =
+                    #    super.selective.overrideAttrs (oldAttrs:
+                    #      { doCheck = false;
+                    #        configureFlags =
+                    #          oldAttrs.configureFlags ++ ["--disable-tests"];
+                    #      });
+                };
+        };
+
+    hspkgs = mkHaskellPackages true;
+
+    additionalDeps = hspkgs.mkDerivation rec {
+              version = "0.1";
+              pname   = "streamly-examples-additional";
+              license = "BSD-3-Clause";
+
+              setupHaskellDepends = with hspkgs; [
+                cabal-doctest
+              ];
+              executableFrameworkDepends = with hspkgs;
+                # XXX On macOS cabal2nix does not seem to generate a
+                # dependency on Cocoa framework.
+                if builtins.currentSystem == "x86_64-darwin"
+                then [nixpkgs.darwin.apple_sdk.frameworks.Cocoa]
+                else [];
+            };
+
+    shell = hspkgs.shellFor {
+        packages = p:
+          [ p.streamly-examples
+            additionalDeps
+          ];
+        shellHook = ''
+          if test -n "$PS_SHELL"
+          then
+            export PS1="$PS_SHELL\[$bldred\](nix)\[$txtrst\] "
+          fi
+        '';
+    };
+in if nixpkgs.lib.inNixShell
+   then shell
+   else (mkHaskellPackages false).streamly
