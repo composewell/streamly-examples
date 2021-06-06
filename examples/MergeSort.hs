1f033bdb02267cfe1c542a2b208de0f025deaac3
Harendra Kumar <harendra@composewell.com>
2021-06-06 15:52:14 +0530

    Simplify, do not use stream monad

--- examples/MergeSort.hs
+++ examples/MergeSort.hs
@@ -1,23 +1,26 @@
 {-# LANGUAGE FlexibleContexts    #-}
 
--- This example generates two streams sorted in ascending order and merges
--- them in ascending order, concurrently.
+-- This example generates two random streams sorted in ascending order and
+-- merges them in ascending order, concurrently.
 --
 -- Compile with '-threaded -with-rtsopts "-N"' GHC options to use the
 -- parallelism.
 
-import Data.List (sort)
 import Data.Word (Word16)
+import Streamly.Prelude (SerialT)
 import System.Random (getStdGen, randoms)
 
-import Streamly.Prelude (Serial)
+import qualified Data.List as List
 import qualified Streamly.Prelude as Stream
 
-getSorted :: Serial Word16
-getSorted = do
-    g <- Stream.fromEffect getStdGen
+getRandomSorted :: IO (SerialT IO Word16)
+getRandomSorted = do
+    g <- getStdGen
     let ls = take 100000 (randoms g) :: [Word16]
-    foldMap return (sort ls)
+    return $ Stream.fromList (List.sort ls)
 
 main :: IO ()
-main = Stream.last (Stream.mergeAsyncBy compare getSorted getSorted) >>= print
+main = do
+    s1 <- getRandomSorted
+    s2 <- getRandomSorted
+    Stream.last (Stream.mergeAsyncBy compare s1 s2) >>= print
