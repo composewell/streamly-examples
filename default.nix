0926cd29365671ce91c1dd3bce21eb52b320c194
Harendra Kumar <harendra@composewell.com>
2021-10-09 11:59:10 +0530

    Add unicode-data package in the package set

--- default.nix
+++ default.nix
@@ -72,6 +72,13 @@ let haskellPackages =
                           sha256 = "073wbhdxj1sh5160blaihbzkkhabs8s71pqhag16lvmgbb7a3hla";
                         } {};
 
+                    unicode-data =
+                      super.callHackageDirect
+                        { pkg = "unicode-data";
+                          ver = "0.1.0.1";
+                          sha256 = "0zwglwfpfbavsp3sjq3kb5dqj0wj1asf3zgj23silfarkr8mmyav";
+                        } {};
+
                     # Example to Use a different version of a package
                     #QuickCheck = self.QuickCheck_2_14;
 
