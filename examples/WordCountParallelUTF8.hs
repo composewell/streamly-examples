cc1358a418b13942b3330667ab042db023906e36
Adithya Kumar <adithya@composewell.com>
2021-11-22 15:53:42 +0530

    Update WordCountParallelUTF8 to compile with streamly master

--- examples/WordCountParallelUTF8.hs
+++ examples/WordCountParallelUTF8.hs
@@ -46,7 +46,7 @@ import qualified Streamly.Internal.Unicode.Stream as Unicode
        (DecodeState, DecodeError(..), CodePoint, decodeUtf8Either
        , resumeDecodeUtf8Either)
 import qualified Streamly.Internal.Data.Array.Foreign.Mut.Type as MArray
-       (unsafeIndexIO, unsafeWriteIndex, Array, newArray)
+       (getIndexUnsafe, putIndexUnsafe, modifyIndexUnsafe, Array, newArray)
 
 
 -------------------------------------------------------------------------------
@@ -179,16 +179,15 @@ data Field =
 -------------------------------------------------------------------------------
 
 readField :: MArray.Array Int -> Field -> IO Int
-readField v fld = MArray.unsafeIndexIO v (fromEnum fld)
+readField v fld = MArray.getIndexUnsafe v (fromEnum fld)
 
 writeField :: MArray.Array Int -> Field -> Int -> IO ()
-writeField v fld = void . MArray.unsafeWriteIndex v (fromEnum fld)
+writeField v fld = void . MArray.putIndexUnsafe v (fromEnum fld)
 
 modifyField :: MArray.Array Int -> Field -> (Int -> Int) -> IO ()
 modifyField v fld f = do
   let index = fromEnum fld
-  val <- MArray.unsafeIndexIO v index
-  void $ MArray.unsafeWriteIndex v index (f val)
+  MArray.modifyIndexUnsafe v index (\x -> (f x, ()))
 
 newCounts :: IO (MArray.Array Int)
 newCounts = do
