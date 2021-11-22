2fc97203754af1a1a955ef2361f3881eb6e9ed3b
Adithya Kumar <adithya@composewell.com>
2021-11-22 15:53:20 +0530

    Update Split to compile with streamly master

--- examples/Split.hs
+++ examples/Split.hs
@@ -1,13 +1,17 @@
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.State.Strict (StateT(..), get, put)
 import Data.Function ((&))
+import Data.Word (Word8)
 import System.Environment (getArgs)
 import System.IO (Handle, IOMode(..), openFile, hClose)
+import Streamly.Internal.Data.Stream.IsStream.Type (toStreamD, fromStreamD)
+import Streamly.Prelude (SerialT)
 
 import qualified Streamly.Prelude as Stream
-import qualified Streamly.Internal.Data.Stream.IsStream as Stream (chunksOf2)
+import qualified Streamly.Internal.Data.Stream.StreamD as StreamD (refoldMany)
+import qualified Streamly.Internal.Data.Refold.Type as Refold (take)
 import qualified Streamly.FileSystem.Handle as Handle
-import qualified Streamly.Internal.FileSystem.Handle as Handle (write2)
+import qualified Streamly.Internal.FileSystem.Handle as Handle (consumer)
 
 newHandle :: StateT (Maybe (Handle, Int)) IO Handle
 newHandle = do
@@ -25,12 +29,17 @@ newHandle = do
 -- of directory names.
 splitFile :: Handle -> IO ()
 splitFile inHandle =
-      Stream.unfold Handle.read inHandle -- SerialT IO Word8
+      (Stream.unfold Handle.read inHandle :: SerialT IO Word8) -- SerialT IO Word8
     & Stream.liftInner -- SerialT (StateT (Maybe (Handle, Int)) IO) Word8
-    & Stream.chunksOf2 (180 * 1024 * 1024) newHandle Handle.write2 -- SerialT (StateT (Maybe (Handle, Int)) IO) ()
+    & chunksOf2 (180 * 1024 * 1024) newHandle Handle.consumer -- SerialT (StateT (Maybe (Handle, Int)) IO) ()
     & Stream.runStateT (return Nothing)  -- SerialT IO (Maybe (Handle, Int), ())
     & Stream.map snd                     -- SerialT IO ()
     & Stream.drain                       -- SerialT IO ()
+
+    where
+
+    chunksOf2 n i f =
+        fromStreamD . StreamD.refoldMany (Refold.take n f) i . toStreamD
 
 main :: IO ()
 main = do
