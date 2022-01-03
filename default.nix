4a8d76958bd97f1580452ad8eba50738592c078b
adithyaov <adithya@composewell.com>
2022-01-03 23:39:32 +0530

    Update streamly and add unicode-data in default.nix

--- default.nix
+++ default.nix
@@ -53,8 +53,8 @@ let haskellPackages =
                       nixpkgs.haskell.lib.overrideCabal
                         (super.callHackageDirect
                           { pkg = "streamly";
-                            ver = "0.8.0";
-                            sha256 = "0vy2lkljizlhpbpbybmg9jcmj2g4s1aaqd2dzy5c0y0n4rgwxask";
+                            ver = "0.8.1";
+                            sha256 = "0ywyy7gxjnp32hx8kki0lfn94bnc9mzjh8g6mg65ff3vv28k2vdr";
                           } {})
                         (old:
                           { librarySystemDepends =
@@ -64,6 +64,13 @@ let haskellPackages =
                             enableLibraryProfiling = false;
                             doHaddock = false;
                           });
+
+                    unicode-data =
+                      super.callHackageDirect
+                        { pkg = "unicode-data";
+                          ver = "0.2.0";
+                          sha256 = "14crb68g79yyw87fgh49z2fn4glqx0zr53v6mapihaxzkikhkkc3";
+                        } {};
 
                     fusion-plugin =
                       super.callHackageDirect
