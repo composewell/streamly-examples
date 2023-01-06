74be3f42c0d9ae61aa428d38233e4fe7ccf5959e
Ranjeet Kumar Ranjan <ranjeetr@composewell.com>
2023-01-06 14:40:45 +0530

    Fix review comments

--- examples/ListDir.hs
+++ examples/ListDir.hs
@@ -4,19 +4,17 @@
 module Main (main) where
 
 import Data.Function ((&))
+import Streamly.Internal.FileSystem.Dir (readEitherPaths)
 import System.IO (stdout, hSetBuffering, BufferMode(LineBuffering))
 
 import qualified Streamly.Data.Fold as Fold
 import qualified Streamly.Data.Stream.Prelude as Stream
-import qualified Streamly.Internal.Data.Stream.Concurrent as Concur (ahead2)
-import qualified Streamly.Internal.Data.Stream as Stream (concatIterateWith)
-import qualified Streamly.Internal.FileSystem.Dir as Dir (readEitherPaths)
 
 -- | List the current directory recursively using concurrent processing
 main :: IO ()
 main = do
     hSetBuffering stdout LineBuffering
     let start = Stream.fromPure (Left ".")
-        mapper = either Dir.readEitherPaths (const Stream.nil)
-    Stream.concatIterateWith Concur.ahead2 mapper start
-        & Stream.fold (Fold.drainBy print)
+        mapper = either readEitherPaths (const Stream.nil)
+    Stream.parConcatIterate id mapper start
+        & Stream.fold (Fold.drainMapM print)
