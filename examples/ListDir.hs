a99ffcf76452a6bf60653fa5b41fe0662b6f757a
Harendra Kumar <harendra@composewell.com>
2023-01-11 17:37:04 +0530

    Add different variants of directory traversal

--- examples/ListDir.hs
+++ examples/ListDir.hs
@@ -3,18 +3,34 @@
 
 module Main (main) where
 
-import Data.Function ((&))
-import Streamly.Internal.FileSystem.Dir (readEitherPaths)
 import System.IO (stdout, hSetBuffering, BufferMode(LineBuffering))
 
 import qualified Streamly.Data.Fold as Fold
 import qualified Streamly.Data.Stream.Prelude as Stream
+-- import qualified Streamly.Internal.Data.Stream as Stream
+-- import qualified Streamly.Internal.Data.Unfold as Unfold (either, nil)
+import qualified Streamly.Internal.FileSystem.Dir as Dir
 
 -- | List the current directory recursively using concurrent processing
 main :: IO ()
 main = do
     hSetBuffering stdout LineBuffering
-    let start = Stream.fromPure (Left ".")
-        mapper = either readEitherPaths (const Stream.nil)
-    Stream.parConcatIterate id mapper start
-        & Stream.fold (Fold.drainMapM print)
+    Stream.fold (Fold.drainMapM print)
+        -- $ Stream.unfoldIterateDfs unfoldOne
+        -- $ Stream.unfoldIterateBfs unfoldOne
+        -- $ Stream.unfoldIterateBfsRev unfoldOne
+        -- $ Stream.concatIterateDfs streamOneMaybe
+        -- $ Stream.concatIterateBfs streamOneMaybe
+        -- $ Stream.concatIterateBfsRev streamOneMaybe
+        -- $ Stream.concatIterateWith Stream.append streamOne
+        -- $ Stream.mergeIterateWith Stream.interleave streamOne
+        $ Stream.parConcatIterate id streamOne
+        -- $ Stream.parConcatIterate (Stream.interleaved True) streamOne
+        -- $ Stream.parConcatIterate (Stream.ordered True) streamOne
+        $ Stream.fromPure (Left ".")
+
+    where
+
+    -- unfoldOne = Unfold.either Dir.eitherReaderPaths Unfold.nil
+    -- streamOneMaybe = either (Just . Dir.readEitherPaths) (const Nothing)
+    streamOne = either Dir.readEitherPaths (const Stream.nil)
