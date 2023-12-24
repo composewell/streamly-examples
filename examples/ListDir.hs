1a6e557927fb2ad6509cf762d17bc903d0d8b2ab
Harendra Kumar <harendra@composewell.com>
2023-12-24 05:40:07 +0530

    Use streamly native Path type

--- examples/ListDir.hs
+++ examples/ListDir.hs
@@ -1,20 +1,9 @@
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE BangPatterns #-}
-{-# LANGUAGE QuasiQuotes #-}
-{-# LANGUAGE MagicHash #-}
-{-# LANGUAGE PackageImports #-}
 {-# OPTIONS_GHC -Wno-deprecations #-}
 
 module Main (main) where
 
-import Data.Word (Word8)
 import System.IO (stdout, hSetBuffering, BufferMode(LineBuffering))
-import System.OsPath (osp)
-import "filepath" System.OsString.Internal.Types (getOsString, getPosixString)
-import System.OsString.Data.ByteString.Short (ShortByteString(..))
-import Streamly.Internal.Data.MutByteArray (MutByteArray(..))
-import Streamly.Internal.Data.Array (Array(..))
-import GHC.Exts (sizeofByteArray#, Int(..), unsafeCoerce#)
 
 import qualified Streamly.Data.Array as Array
 import qualified Streamly.Data.Stream.Prelude as Stream
@@ -22,6 +11,7 @@ import qualified Streamly.Internal.Data.Stream as Stream
 -- import qualified Streamly.Internal.Data.Unfold as Unfold (either, nil)
 import qualified Streamly.Internal.FileSystem.Dir as Dir
 import qualified Streamly.Internal.FileSystem.Handle as Handle
+import qualified Streamly.Internal.FileSystem.Path as Path
 
 -- | List the current directory recursively using concurrent processing
 main :: IO ()
@@ -29,7 +19,7 @@ main = do
     hSetBuffering stdout LineBuffering
     Stream.fold (Handle.writeWith 32000 stdout)
         $ Stream.interposeSuffix 10 Array.reader
-        $ fmap (osPathToArray . either id id)
+        $ fmap (Path.toByteArray . either id id)
 
         -- Serial using unfolds (fastest serial)
         -- $ Stream.unfoldIterateDfs unfoldOne
@@ -49,7 +39,7 @@ main = do
         $ Stream.parConcatIterate id streamOne
         -- $ Stream.parConcatIterate (Stream.interleaved True) streamOne
         -- $ Stream.parConcatIterate (Stream.ordered True) streamOne
-        $ Stream.fromPure (Left [osp|.|])
+        $ Stream.fromPure (Left $ Path.fromString ".")
         -- $ Stream.fromPure (Left ".")
 
     where
@@ -57,7 +47,3 @@ main = do
     -- unfoldOne = Unfold.either Dir.eitherReaderPaths Unfold.nil
     -- streamOneMaybe = either (Just . Dir.readEitherPaths) (const Nothing)
     streamOne = either Dir.readEitherPaths (const Stream.nil)
-    osPathToArray p =
-        let !(SBS barr#) = getPosixString $ getOsString p
-            mbarr = MutByteArray (unsafeCoerce# barr#)
-          in Array mbarr 0 (I# (sizeofByteArray# barr#)) :: Array Word8
