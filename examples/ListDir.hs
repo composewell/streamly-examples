446e2a8a7115550d5f6acc21694c1f0f2816475d
Harendra Kumar <harendra@composewell.com>
2024-01-10 12:03:37 +0530

    Use readEitherChunks

--- examples/ListDir.hs
+++ examples/ListDir.hs
@@ -1,14 +1,20 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE QuasiQuotes #-}
 {-# OPTIONS_GHC -Wno-deprecations #-}
+{-# OPTIONS_GHC -ddump-simpl -dsuppress-all -ddump-to-file #-}
+
 
 module Main (main) where
 
+import Data.Maybe (fromJust)
 import System.IO (stdout, hSetBuffering, BufferMode(LineBuffering))
+import System.OsPath (osp)
 
-import qualified Streamly.Data.Array as Array
+import qualified Streamly.Internal.Data.Array as Array
+import qualified Streamly.Internal.Data.Fold as Fold
 import qualified Streamly.Data.Stream.Prelude as Stream
 import qualified Streamly.Internal.Data.Stream as Stream
--- import qualified Streamly.Internal.Data.Unfold as Unfold (either, nil)
+import qualified Streamly.Internal.Data.Unfold as Unfold
 import qualified Streamly.Internal.FileSystem.Dir as Dir
 import qualified Streamly.Internal.FileSystem.Handle as Handle
 import qualified Streamly.Internal.FileSystem.Path as Path
@@ -17,9 +23,19 @@ import qualified Streamly.Internal.FileSystem.Path as Path
 main :: IO ()
 main = do
     hSetBuffering stdout LineBuffering
+    -- Stream.fold (Fold.drain)
+    -- Stream.fold (Fold.drainMapM print)
     Stream.fold (Handle.writeWith 32000 stdout)
+    -- Stream.fold (Handle.writeChunks stdout)
+    -- Stream.fold (Array.lPinnedCompactGE 32000 (Handle.writeChunks stdout))
         $ Stream.interposeSuffix 10 Array.reader
-        $ fmap (Path.toByteArray . either id id)
+        -- $ Array.compactInterposeGE 10 32000
+        -- $ Array.pinnedCompactLE 32000
+        $ fmap Path.toChunk
+        $ Stream.unfoldMany Unfold.fromList
+        $ fmap (either (:[]) id)
+        -- $ fmap Path.toChunk
+        -- $ Stream.catLefts
 
         -- Serial using unfolds (fastest serial)
         -- $ Stream.unfoldIterateDfs unfoldOne
@@ -36,14 +52,20 @@ main = do
         -- $ Stream.mergeIterateWith Stream.interleave streamOne
 
         -- Concurrent
-        $ Stream.parConcatIterate id streamOne
+        -- XXX To reduce concurrency overhead, perform buffering in each worker
+        -- and post the buffer or return [Path] and then unfold it.
+        $ Stream.parConcatIterate
+            -- ( Stream.eager False . Stream.maxBuffer 1000 . Stream.maxThreads 4)
+            id
+            streamOne
         -- $ Stream.parConcatIterate (Stream.interleaved True) streamOne
         -- $ Stream.parConcatIterate (Stream.ordered True) streamOne
-        $ Stream.fromPure (Left $ Path.fromString ".")
+        $ Stream.fromPure (Left $ fromJust $ Path.fromString ".")
         -- $ Stream.fromPure (Left ".")
+        -- $ Stream.fromPure (Left [osp|.|])
 
     where
 
     -- unfoldOne = Unfold.either Dir.eitherReaderPaths Unfold.nil
     -- streamOneMaybe = either (Just . Dir.readEitherPaths) (const Nothing)
-    streamOne = either Dir.readEitherPaths (const Stream.nil)
+    streamOne = either Dir.readEitherChunks (const Stream.nil)
